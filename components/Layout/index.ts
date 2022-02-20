eb94deed71c2370e8878a96ae20c4a5ab555c7d6
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-02-20 18:37:12 +0530

    chore: hope this works

--- components/Layout/index.ts
+++ components/Layout/index.ts
@@ -1,3 +1,3 @@
-export { default as Header } from '@/components/layout/Header';
-export { default as Footer } from '@/components/layout/Footer';
-export { default as Layout } from '@/components/layout/Layout';
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as Layout } from './Layout';
