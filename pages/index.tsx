96b10ac3da75cba9a41720ac8e75036bbe7932f1
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-02-20 19:41:13 +0530

    chore: production hope

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
-
+import Layout from '../components/layout/Layout';
 import { Hero, Features, Pricing, Team, BlogCards } from '@/components/home';
-import Layout from '@/components/layout/Layout';
 
 const Home: NextPage = () => {
   return (
