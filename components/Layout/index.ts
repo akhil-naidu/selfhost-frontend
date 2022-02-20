4afb09388258cfa36b2934cc02ad7165402c5496
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-02-20 18:27:50 +0530

    test: 01

--- components/Layout/index.ts
+++ components/Layout/index.ts
@@ -1,3 +1,5 @@
-export { default as Header } from '@/components/layout/Header';
-export { default as Footer } from '@/components/layout/Footer';
-export { default as Layout } from '@/components/layout/Layout';
+import { default as Header } from './Header';
+import { default as Footer } from './Footer';
+import { default as Layout } from './Layout';
+
+export { Header, Footer, Layout };
