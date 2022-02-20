b21bba469c17b4ff339784ebff850161279b728b
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-02-20 20:23:22 +0530

    fix: dark mode

--- components/home/BlogCards.tsx
+++ components/home/BlogCards.tsx
@@ -12,13 +12,13 @@ const content = [
     id: 2,
     img: 'https://res.cloudinary.com/leewardslope/image/upload/v1643508553/selfhost/learn_tailwindcss_2263a75a7f.jpg',
     title: 'Step by Step tutorial on How to Install Forem',
-    excerpt: 'A small blog post with a video guide',
+    excerpt: 'A Video note on self-hosting Forem on your own digital ocean server.',
   },
 ];
 
 const BlogCards = () => {
   return (
-    <div className='my-12'>
+    <div className=' bg-white p-8 dark:bg-gray-800'>
       <h1 className='text-center text-3xl font-semibold capitalize text-gray-800 dark:text-white lg:text-4xl'>
         Checkout <br />
         our Latest <span className='text-blue-600'>Posts</span>
@@ -30,17 +30,17 @@ const BlogCards = () => {
         <span className='inline-block h-1 w-1 rounded-full bg-blue-600'></span>
       </div>
 
-      <section className=' bg-white dark:bg-gray-900 lg:flex lg:justify-center lg:pt-12'>
+      <section className='lg:flex lg:justify-center lg:pt-12'>
         {content.map((blog) => (
           <div key={blog.id}>
-            <div className='relative bg-gray-100 dark:bg-gray-800  lg:mx-8 lg:flex lg:max-w-2xl lg:rounded-lg lg:shadow-lg'>
+            <div className='relative bg-gray-100 dark:bg-gray-900 lg:mx-8 lg:flex lg:rounded-lg lg:shadow-lg'>
               <div className='lg:w-1/2'>
                 <div className='relative overflow-hidden object-center lg:h-full lg:w-full lg:rounded-l-lg'>
                   <Image src={blog.img} alt='' layout='fill' />
                 </div>
               </div>
 
-              <div className='max-w-xl px-6 lg:w-1/2 lg:max-w-5xl lg:p-4'>
+              <div className='max-w-xl px-6 lg:w-1/2 lg:p-4'>
                 <h2 className='text-xl font-bold text-gray-800 dark:text-white md:text-xl'>
                   {blog.title}
                 </h2>
