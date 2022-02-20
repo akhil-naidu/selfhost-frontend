15b61e91ee7b5f277009e7bcf4e4534c7e2cd54b
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-02-20 19:38:01 +0530

    chore: production ready

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,9 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
 
-import { Header, Footer } from './index';
+import Footer from './Footer';
+import Header from './Header';
+
 interface LayoutProps {
   headerContent?: {
     title: string;
