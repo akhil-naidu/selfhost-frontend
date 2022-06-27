bc6ce2e1edb7b986b2950fee4b241ed8ae8573c5
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2022-06-27 11:41:46 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,12 @@ import { NhostApolloProvider } from '@nhost/react-apollo';
 import { nhost } from '@/utils/nhost';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  <Head>
-    <meta name="cf-2fa-verify" content="tgDWriVhnJkrhWBJkvX63g23JHiv6N">
-  </Head>
   return (
     <NhostAuthProvider nhost={nhost}>
       <NhostApolloProvider nhost={nhost}>
+         <Head>
+          <meta name="cf-2fa-verify" content="tgDWriVhnJkrhWBJkvX63g23JHiv6N">
+         </Head>
         <Component {...pageProps} />
       </NhostApolloProvider>
     </NhostAuthProvider>
